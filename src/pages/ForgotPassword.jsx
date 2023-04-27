cc627b22ae4d30accea8af47fb6e6c4b57aa9c4a
alexyswyne <128330363+alexyswyne@users.noreply.github.com>
2023-04-27 11:32:29 -0400

    Update forgotpassword.jsx

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { usePasswordReset } from "../hooks/usePasswordReset";
 
-export default function ForgotPassword() {
-  return <button>Forgot Password</button>;
-}
+const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const { error, passwordreset } = usePasswordReset();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    passwordreset(email);
+  };
+
+  return (
+    <div>
+      <h2>Password Reset</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>email:</span>
+          <input
+            required
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <button type="submit">Reset Password</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default ForgotPassword;
