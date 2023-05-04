506a5ee63ea44701454125a472e299ab0be50a06
Owen McCall <ocwmccall@gmail.com>
2023-05-04 13:36:22 -0400

    Getting API Key error

--- src/pages/MoveItems.jsx
+++ src/pages/MoveItems.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Header from '../components/Header.jsx'
+import '../styles/MoveItems.css'
 
 export default function MoveItems() {
   return (
-    <div>MoveItems</div>
+    <><Header></Header>
+    <div className="main_container">
+      <div className="main_content">
+        
+      </div>
+    </div></>
   )
 }
