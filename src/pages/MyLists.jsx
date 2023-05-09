31171db62342b55fc2fc2010dde8c3735961bbb4
Kervcodes <kervintz84@hotmail.com>
2023-05-09 19:35:36 -0400

    wip style for mylist

--- src/pages/MyLists.jsx
+++ src/pages/MyLists.jsx
@@ -3,8 +3,9 @@ import React, { useState, useEffect } from 'react';
 import { db } from '../firebase'
 import { collection, getDocs, addDoc, deleteDoc, updateDoc, deleteField, query, where } from 'firebase/firestore';
 import Lists from './Lists';
+import styled from 'styled-components';
 
-export default function MyLists({ user }) {
+function MyLists({ user }) {
   const [lists, setLists] = useState(null);
   const [name, setName] = useState("");
 
@@ -47,14 +48,20 @@ export default function MyLists({ user }) {
   }, [])
 
   return (
-    <div>
-      <p>Previous saved lists</p>
-      <div>
-        <h1>Lists</h1>
-        {
-          lists && <Lists lists={lists} />
-        }
+    <Styling>
+       <div className='container'>
+      <div className="lists">
+        <h2> saved lists</h2>
         <div>
+          <h1>Lists</h1>
+          {
+            lists && <Lists lists={lists} />
+          }
+          
+        </div>
+      </div>
+      <div className="list-operations">
+      <div>
           <form onSubmit={handleNewList}>
             <label>
               <span>New List Name:</span>
@@ -71,6 +78,96 @@ export default function MyLists({ user }) {
           <button>Delete List</button>
         </div>
       </div>
+      
     </div>
+    </Styling>
+   
   )
 }
+
+
+const Styling = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .lists{
+    width: 100%;
+    border: 2px solid red;
+  }
+
+
+  form {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
+  
+  form fieldset {
+    border: none;
+    margin: 0;
+    padding: 0;
+    width: 400px;
+  }
+  
+  .form-label {
+    display: block;
+    margin-bottom: 0.5rem;
+    font-size: 1.2rem;
+    font-weight: bold;
+    letter-spacing: .5px;
+  }
+  
+  .form-input:hover{
+    border-bottom: 2px solid #ef476f;
+  }
+  
+  #login {
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
+  
+  .buttons {
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    font-size: .7rem;
+    font-weight: bold;
+  }
+  
+  .buttons button {
+    width: 200px;
+    margin-top: 0.5rem;
+    padding: 0.5rem 1rem;
+    border-radius: 0.25rem;
+    font-size: 1rem;
+    background-color: #ffd166;
+    color: #fff;
+    border: none;
+  }
+  
+  .buttons button:hover {
+    background: #ef476f;
+    color: #fff;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out
+  }
+  .form-input{
+    width: 100%;
+    padding: 5px 10px;
+    border-bottom: 1px solid #3d3935;
+    border-radius: 4px;
+    background-color: #fff;
+    color: #3d3935;
+    outline: 0;
+    transition: all 0.5s linear;
+  }
+
+  li:nth-child(odd){
+    background: rgba(24, 138, 178, .2);
+  }
+`
+export default MyLists;
