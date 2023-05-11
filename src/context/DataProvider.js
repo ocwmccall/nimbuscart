f8c1c920d964f3f9542811d81a8babeebd2e338a
Owen McCall <ocwmccall@gmail.com>
2023-05-11 11:53:05 -0400

    Update DataProvider.js

--- src/context/DataProvider.js
+++ src/context/DataProvider.js
@@ -5,9 +5,10 @@ import DataContext from "./DataContext";
 
 const DataProvider = ({ children }) => {
     const [selectedId, setSelectedId] = useState(null);
+    const [ rowSelection, setRowSelection] = useState({});
 
     return (
-        <DataContext.Provider value={{ selectedId, setSelectedId }} >
+        <DataContext.Provider value={{ selectedId, setSelectedId, rowSelection, setRowSelection } } >
             {children}
         </DataContext.Provider>
     )
